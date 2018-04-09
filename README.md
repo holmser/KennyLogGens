f36e01d12fd2babb859a5c9a55fb202204857e06
Chris Holmes <chlmes@amazon.com>
2018-04-09 09:43:19 -0600

    fix readme

--- README.md
+++ README.md
@@ -1,5 +1,24 @@
-# KennyLoggins
+# KennyLogGens
 
 ![Kenny Loggins](images/kennyloggins.jpg)
 
-Log generator for testing log shipping and written in Go.
+```
+	KennyLoggins is a utility for generating log files for testing log
+	shipping systems.  Welcome to the Danger Zone.
+
+Usage:
+  klog [flags]
+  klog [command]
+
+Available Commands:
+  gen         Generate logs to syslog
+  help        Help about any command
+
+Flags:
+      --config string   config file (default is $HOME/.tmp.yaml)
+  -h, --help            help for klog
+  -t, --toggle          Help message for toggle
+
+Use "klog [command] --help" for more information about a command.
+
+```
--- cmd/gen.go
+++ cmd/gen.go
@@ -29,13 +29,8 @@ import (
 // genCmd represents the gen command
 var genCmd = &cobra.Command{
 	Use:   "gen",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Generate logs to syslog",
+	Long:  `This will alternately spew the lyrics to Danny's Song and Danger Zone to syslog. `,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("gen called")
 	},
